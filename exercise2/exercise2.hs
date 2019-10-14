1f548dc6697b0d21c9d210e7441def01a99444b4
Gagah Pangeran <gagahpangeran@gmail.com>
2019-10-14 11:15:30 +0700

    add solution number 2 and 3 exercise 2

--- exercise2/exercise2.hs
+++ exercise2/exercise2.hs
@@ -1,2 +1,11 @@
 -- Number 1
 num1 = [x+y | x <- [1..4], y <- [2..4], x > y]
+
+-- Number 2
+divisor n = [x | x <- [1 .. n], n `mod` x == 0]
+
+-- Number 3
+quickSort [] = []
+quickSort (x:xs) = quickSort [y | y <- xs, y <= x]
+                   ++ [x] ++
+                   quickSort [y | y <- xs, y > x]
